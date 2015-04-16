e1bcffeac7c31851a0dfe6fd037ed1eb94050000
William Bengtsson <wilbe075@student.liu.se>
2015-04-16 19:48:31 +0200

    Added home state with ui-router

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,35 @@
-angular.module('pmt', [])
+angular.module('pmt', ['ui.router'])
+.config([
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: '/home.html',
+        controller: 'MainCtrl'
+      });
+
+    $urlRouterProvider.otherwise('home');
+  }
+])
+
 .factory('timeElements', [function(){ 
   var o = {
-    timeElements: []
+    elements: []
   }; 
   return o;
 }])
+
 .controller('MainCtrl', [
   '$scope', 
   'timeElements',
   function($scope, timeElements){ 
-    $scope.timeElements = timeElements.timeElements; 
+    $scope.timeElements = timeElements.elements; 
     $scope.addTimeElement = function(){   	
-    	$scope.timeElements.push({
-    		date: $scope.date,
+      $scope.timeElements.push({
+        date: $scope.date,
         name: $scope.name, 
     		start: $scope.start,
     		end: $scope.end
