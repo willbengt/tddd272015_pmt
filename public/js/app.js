10b79ca968ddbf81b02fa30a8f3290036928bb7b
William Bengtsson <wilbe075@student.liu.se>
2015-04-17 18:27:53 +0200

    Added some comments in the code and added timeelments state for editing the time elements on the edit page

--- public/js/app.js
+++ public/js/app.js
@@ -13,30 +13,37 @@ angular.module('pmt', ['ui.router'])
       })
 
       .state('timeelements', {
-        url: '/timeelements/{id}',
+        url: '/timeelements/{id}', // 'id' is a route parameter that will be made available to EditCtrl.
         templateUrl: '/timeelements.html',
         controller: 'EditCtrl'
       })
 
-    $urlRouterProvider.otherwise('home');
+    $urlRouterProvider.otherwise('home'); //if the app receives a URL that is not defined it is redirected to 'home'
   }
 ])
 
-.factory('timeElements', [function(){ 
-  var o = {
-    elements: []
+.factory('timeElements', [function(){ //instead of storing our data in our controller, we can use a service to organize and share code across the app. The service factory function generates the single object or function that represents the service to the rest of the application.
+  var o = { //we create an object - 'o'
+    elements: []  // the array 'elements' is one property of o. 
   }; 
   return o;
 }])
 
 .controller('ReportCtrl', [
-  '$scope', 
+  '$scope', //$scope is the application object (the owner of application variables and functions).
   'timeElements',
-  function($scope, timeElements){ 
-    $scope.timeElements = timeElements.elements;
-    //console.log($scope.timeElements);
-    //$scope.timeElement = timeElements.elements[$stateParams.id];
-    $scope.addTimeElement = function(){   	
+  function($scope, timeElements){ //we inject our timeElements service by adding it as a parameter
+    $scope.timeElements = timeElements.elements; //$scope.timeElements in our controller is binded to the elements array in our service. Any change or modification made to $scope.timeElements will be stored in the service.
+
+    $scope.timeElements.push({ //for testing. just one timeElement as an example. 
+      date: '2015-04-16', 
+      name: 'William', 
+      start: "09:00", 
+      end: "10:00"
+    });
+
+    $scope.addTimeElement = function(){  
+      console.log("$scope.date: " + $scope.date); 	
       $scope.timeElements.push({
         date: $scope.date,
         name: $scope.name, 
@@ -52,19 +59,14 @@ angular.module('pmt', ['ui.router'])
   '$stateParams',
   'timeElements',
   function($scope, $stateParams, timeElements) {
-      $scope.timeElement = timeElements.elements[$stateParams.id];
-  /*
-      $scope.addComment = function(){
-          if($scope.body === '') { return; }
-          $scope.post.comments.push({
-              body: $scope.body,
-              author: 'user',
-              upvotes: 0
-          });
-          $scope.body = '';
-      };
-
-  */
+    $scope.timeElement = timeElements.elements[$stateParams.id]; //$stateParams is used to retrieve the id from the URL to load the appropriate element.
+    
+    $scope.editTimeElement = function(){
+      if(angular.isDefined($scope.date)){$scope.timeElement.date = $scope.date;}
+      if(angular.isDefined($scope.name)){$scope.timeElement.name = $scope.name;}
+      if(angular.isDefined($scope.start)){$scope.timeElement.start = $scope.start;}
+      if(angular.isDefined($scope.end)){$scope.timeElement.end = $scope.end;}
+    };
   }
 ]);
 
