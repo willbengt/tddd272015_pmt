679e3a0dd24b0f1fe22e769ac632d8d38f09491f
William Bengtsson <wilbe075@student.liu.se>
2015-05-03 17:12:38 +0200

    Added validations for Report model

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,2 +1,6 @@
 class Report < ActiveRecord::Base
+	validates :date, :presence => {:message => "^Date can't be blank"}, :format => /\d{2}\/\d{2}\/\d{4}/
+  validates :name, :presence => true
+  validates :starttime, :presence => true
+  validates :endtime, :presence => true
 end
