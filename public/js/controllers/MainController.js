de2d17735df6a1449e4f1259b9ee02e5381dfe61
William Bengtsson <wilbe075@student.liu.se>
2015-05-06 23:10:22 +0200

    Removed some redundant code in Maincontroller

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -1,17 +1,4 @@
 app.controller('MainController', ['$scope', '$http', function($scope, $http){
-    $scope.name = "Time Reporting shit"
-    $scope.header ="header with no other purpose"
-    $scope.subheader="subheader for now"
-    $scope.tableInformation = []
-    $scope.timereport = [
-        {
-            name: "name",
-            proj: "proj",
-            time: "time",
-            text: "text"
-
-        }]
-
     $scope.fetchData = function() {
         $http.get('/fetchdata').
             success(
@@ -23,19 +10,6 @@ app.controller('MainController', ['$scope', '$http', function($scope, $http){
             $scope.subheader = "Bad response"
         );
     }
-
-    $scope.inputData = function(){
-        $http.post('/postdata').
-            success(
-            function(name, proj, time, text){
-                $scope.subheader = "some kind of success"
-            }
-        ).
-            error(
-            $scope.subheader = "nah, this aint working perfect"
-        );
-    }
-
 }]);
 
 app.controller('AddReportController', ['$http', function($http){
