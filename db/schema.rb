0cd0ac5b1c8e2665b9601575c90051bb0903cff9
William Bengtsson <wilbe075@student.liu.se>
2015-05-07 11:17:51 +0200

    Migrated database for dev env and did a bundle install

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20150505115322) do
 
   create_table "reports", force: :cascade do |t|
-    t.string   "title"
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "number"
