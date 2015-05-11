4f5a20c04548b28e8409dd165dd23eb79470a2dc
William Bengtsson <wilbe075@student.liu.se>
2015-05-11 21:21:41 +0200

    Added home state

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,16 @@
-var app = angular.module("TimeReportApp", []);
+var app = angular.module("TimeReportApp", ['ui.router']).config([
+	'$stateProvider',
+	'$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+
+	  $stateProvider
+	    .state('home', {
+	      url: '/home',
+	      templateUrl: '/views/home.html',
+	      controller: 'MainController'
+	    });
+
+	  $urlRouterProvider.otherwise('home');
+	}])
 
 
