bf1592ba0bd691bfdf3db2434bf1c960545c3aa9
raskl508 <raskl508@student.liu.se>
2015-05-22 10:41:41 +0200

    showing d3Bars in report without any problems now

--- public/js/app.js
+++ public/js/app.js
@@ -20,6 +20,7 @@ var app = angular.module("TimeReportApp", ['ui.router']).config([
 	  $urlRouterProvider.otherwise('home');
 	}]);
 
+/*
 app.directive('helloWorld', function() {
 	return {
 		restrict: 'AE',
@@ -27,11 +28,51 @@ app.directive('helloWorld', function() {
 		template: '<h3>Hello World!!</h3>'
 	};
 });
+*/
 
-app.directive('donutChart', function(){
+app.directive('medianChart', function(){
 	function link(scope, el){
-		d3.select(el[0]).append('svg')
-	}
+		var dataArray=[20, 40, 50, 60, 10, 10, 10];
+		var width = 500;
+		var height = 800;
+
+		var widthScale = d3.scale.linear()
+			.domain([0, 60])
+			.range([0, width]);
+
+		var heighScale = d3.scale.linear()
+			.domain([0,60])
+			.range([0, height]);
+
+		var color = d3.scale.linear()
+			.domain([0, 60])
+			.range(["red", "blue"]);
+
+		var axis = d3.svg.axis()
+			.ticks(5)
+			.scale(widthScale);
+
+		var canvas = d3.select("body")
+			.append("svg")
+			.attr("width", width)
+			.attr("height", height)
+			.append("g")
+			.attr("transform", "translate(20, 0)") //flytta, rotera o sånt
+
+		var bars = canvas.selectAll("rect")
+			.data(dataArray)
+			.enter()
+			.append("rect")
+			.attr("width", function(d) { return widthScale(d); })
+			.attr("height", 50)
+			.attr("fill", function(d) { return color(d) })
+			.attr("y", function(d, i) { return i * 100 });
+
+		canvas.append("g")
+			.attr("transform", "translate(0, 700)")
+			.call(axis);
+
+					}
 	return {
 		link: link,
 		restrict: 'E'
