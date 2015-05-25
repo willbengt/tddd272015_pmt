4e6db7810780c9e7a3db94db947099c7ef5312a8
raskl508 <raskl508@student.liu.se>
2015-05-25 13:39:50 +0200

    can now load data from .json

--- public/js/directive/d3js.js
+++ public/js/directive/d3js.js
@@ -11,28 +11,62 @@ app.directive('helloWorld', function() {
 
 app.directive('burndownChart', function() {
     function link(scope, el) {
-        var dataArray = [3,2,1];
-        var width = 500;
-        var height = 800;
+        d3.json("js/directive/mydata.json", function (data) {
 
+            var canvas = d3.select("body").append("svg")
+                .attr("width", 500)
+                .attr("height", 500);
 
+            canvas.selectAll("rect")
+                .data(data)
+                .enter()
+                .append("rect")
+                .attr("width", function (d) { return d.time * 20; } )
+                .attr("height", 48)
+                .attr("y", function (d, i) { return i * 50; })
+                .attr("fill", "blue");
 
-
-
-        var canvas = d3.select("body")
-            .append("svg")
-            .attr("width", width)
-            .attr("height", height)
-            .append("g")
-            .attr("transform", "translate(200, 20)"); //flytta, rotera o sånt
-
-        var bars = canvas.selectAll("p")
-            .data(dataArray)
-            .enter()
-
-        canvas.append("g")
-            .attr("transform", "translate(0, 700)")
+            canvas.selectAll("text")
+                .data(data)
+                .enter()
+                .append("text")
+                .attr("fill", "white")
+                .attr("y", function (d, i) { return i * 50 + 24; })
+                .text(function (d) {return d.name;})
+        })
     }
+//d3.sum(data)
+/*
+var canvas = d3.select("body")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+        var circle = canvas.append("circle")
+            .attr("cx", 50)
+            .attr("cy", 100)
+            .attr("r", 25);
+
+        var circles = canvas.selectAll("circle")
+            .data(data)
+            .attr("fill", "red")
+            .enter()// skapar DOM av den data som ej redan har ett DOM objekt kopplat itll sig.
+            .append("circle")
+            .attr("cx", 50)
+            .attr("cy", 50)
+            .attr("fill", "green")
+            .attr("r", 25);
+
+    circle.transition()
+        .duration(1500)
+        .attr("cx", 150)
+        .attr("cy", 150)
+        .attr("fill", "pink")
+        .each("end", function(){ d3.select(this).attr("fill", "blue") } );
+
+    }
+  /*/
+
     return {
         link: link,
         restrict: 'E'
