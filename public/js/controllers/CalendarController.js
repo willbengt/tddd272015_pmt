0883b73e684c875f7cddf5232045cbf861a286c0
William Bengtsson <wilbe075@student.liu.se>
2015-09-05 12:27:55 +0200

    Implemented authorization process with OAuth 2.0

--- public/js/controllers/CalendarController.js
+++ public/js/controllers/CalendarController.js
@@ -1,3 +1,15 @@
 app.controller('CalendarController', ['$scope', function($scope){
-	$scope.msg = "CalendarController is running"
+  var CLIENT_ID = '711755136597-5k4ijen3f7j0003088jjimt8knlre2cm.apps.googleusercontent.com';
+
+  var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+
+  $scope.authorize = function() {
+    //Initiates the OAuth 2.0 authorization process
+    gapi.auth.authorize({
+      'client_id': CLIENT_ID, 
+      'immediate': false,
+      'response_type' : "token",
+      'scope': SCOPES, 
+    }); 
+  }
 }]);
