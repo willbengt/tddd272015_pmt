01ddc409a0671fcff5bf490d3ec9f5f8af258626
William Bengtsson <wilbe075@student.liu.se>
2015-09-13 13:28:04 +0200

    IMplemented addForm for PeopleController

--- public/js/controllers/PeopleController.js
+++ public/js/controllers/PeopleController.js
@@ -1,3 +1,10 @@
-app.controller('PeopleController', ['$scope', function($scope){
-    console.log("PeopleController is running");
+app.controller('PeopleController', ['$scope', '$state', function($scope, $state){
+
+    $scope.isCurrentState = $state.is("people");
+
+    $scope.add = function() {
+        console.log($state);
+        console.log(JSON.stringify($scope.person));
+    };
+
 }]);
