40fcc24bc7283cf36fee8b67fbc62c4f378620e9
William Bengtsson <wilbe075@student.liu.se>
2015-09-21 19:10:56 +0200

    Created migration AddProjectToPeople and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150921162802) do
+ActiveRecord::Schema.define(version: 20150921170459) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "project"
   end
 
   create_table "projects", force: :cascade do |t|
