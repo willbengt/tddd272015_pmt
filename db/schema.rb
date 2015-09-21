adf89ce1efd85a5331e6b1ca86c220aa92da9ad7
William Bengtsson <wilbe075@student.liu.se>
2015-09-21 18:30:39 +0200

    Added Project model to database

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150913130143) do
+ActiveRecord::Schema.define(version: 20150921162802) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
