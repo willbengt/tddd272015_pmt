538f89a88e6080c2d53a9c7aa575d64858f90682
William Bengtsson <wilbe075@student.liu.se>
2015-10-31 18:08:44 +0100

    Added display of burndown chart for specific project

--- public/js/controllers/ProjectController.js
+++ public/js/controllers/ProjectController.js
@@ -1,13 +1,27 @@
-app.controller('ProjectController', ['$scope', '$stateParams', 'Project', function($scope, $stateParams, Project){
+app.controller('ProjectController', ['$scope', '$stateParams', '$http', '$filter', 'Project', function($scope, $stateParams, $http, $filter, Project){
   var projectId = $stateParams.projectId;
 
  	$scope.project = [];
 
-  $scope.loadProject = function() {
+  loadProject = function() {
     $scope.project = Project.get({id:projectId}, function() {
       console.log("success (GET http://localhost:3000/api/projects/" + projectId + ")");
     }, function(error) {
       console.log("error (GET http://localhost:3000/api/projects/" + projectId + ")");
     });
+  };
+
+  loadReports = function() {
+  	$http.get('/report').success(function(response) {
+      console.log("success (GET http://localhost:3000/report)");
+      $scope.reports = $filter('filter')(response, {project: $scope.project.id}); 
+    }).error(function() {
+      console.log("error (GET http://localhost:3000/report)");
+    });
+  };
+
+  $scope.init = function() {
+    loadProject();
+    loadReports();
   };
 }]);
