15dfb194c43a070e2ec7a3d77ab84fa7e9a51666
William Bengtsson <wilbe075@student.liu.se>
2015-11-02 21:31:02 +0100

    Added calendarfetch functionality from previous calandar module to import modal

--- public/js/controllers/ProjectController.js
+++ public/js/controllers/ProjectController.js
@@ -1,4 +1,4 @@
-app.controller('ProjectController', ['$scope', '$stateParams', '$http', '$filter', 'Project', function($scope, $stateParams, $http, $filter, Project){
+app.controller('ProjectController', ['$scope', '$stateParams', '$http', '$filter', '$timeout', 'Project', function($scope, $stateParams, $http, $filter, $timeout, Project){
   var projectId = $stateParams.projectId;
 
  	$scope.project = [];
@@ -24,4 +24,84 @@ app.controller('ProjectController', ['$scope', '$stateParams', '$http', '$filter
     loadProject();
     loadReports();
   };
+
+  var CLIENT_ID = '711755136597-5k4ijen3f7j0003088jjimt8knlre2cm.apps.googleusercontent.com';
+  var SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
+  $scope.calendarsFetched = false;
+
+  $scope.calendars = [];
+  listCalendars = function() {
+
+    var request = gapi.client.calendar.calendarList.list({});
+    var singleCalendar = [];
+
+    $scope.calendars = [];
+
+    return request.execute(function(response) {
+
+      for (var i = 0; i < response.items.length; i++) {
+        $scope.calendars.push({
+          title : response.items[i].summary, 
+          id : response.items[i].id
+        }); 
+      }
+      $scope.calendarsFetched = true;
+    });
+  };
+    
+  $scope.calendarEvents = [];         
+  listCalendarEvents = function() {
+    
+    var timeZoneOffset = (new Date()).getTimezoneOffset() * 60000;
+    var timeMin = (new Date($scope.startDate - timeZoneOffset)).toISOString();
+
+    //API: https://developers.google.com/google-apps/calendar/v3/reference/events/list
+    var request = gapi.client.calendar.events.list({ 
+      'calendarId': $scope.calendarSelected,
+      'timeMin' : timeMin,
+      'orderBy': 'startTime'
+    });
+
+    $scope.calendarEvents = []; 
+
+    //API: https://developers.google.com/google-apps/calendar/v3/reference/events#resource
+    request.then(function(response) { 
+      
+      for (i = 0; i < $scope.numberOfEvents; i++) {
+        $scope.calendarEvents.push({
+          title : response.result.items[i].summary, 
+          start : response.result.items[i].start.dateTime, 
+          end : response.result.items[i].end.dateTime
+        });
+      }
+    });
+  };
+
+  $scope.fetchCalendars = function() {
+    return gapi.client.load('calendar', 'v3', listCalendars);
+  };
+
+  $scope.fetchCalendarEvents = function() {
+    return gapi.client.load('calendar', 'v3', listCalendarEvents);
+  };
+
+  $scope.authorize = function() {
+    //Initiates the OAuth 2.0 authorization process
+    gapi.auth.authorize({
+      'client_id': CLIENT_ID, 
+      'immediate': false,
+      'response_type' : "token",
+      'scope': SCOPES, 
+    }); 
+  };
+
+  $scope.openDatePicker = function($event) {
+    $scope.datePickerOpen = true;
+  };
+
+  $scope.dateOptions = {
+    startingDay: 1
+  };
+
+  $scope.datePickerOpen = false;
 }]);
