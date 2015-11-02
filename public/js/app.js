47c336428c66595cdc2bbe90567a205e691c1eae
William Bengtsson <wilbe075@student.liu.se>
2015-11-02 21:29:38 +0100

    Added angular ui-bootstrap in order to use datepicker

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,4 @@
-var app = angular.module("TimeReportApp", ['ui.router', 'angular-oauth2', 'xeditable', 'ngResource']).config([
+var app = angular.module("TimeReportApp", ['ui.router', 'angular-oauth2', 'xeditable', 'ngResource', 'ui.bootstrap']).config([
 	'$stateProvider',
 	'$urlRouterProvider',
 	function($stateProvider, $urlRouterProvider) {
