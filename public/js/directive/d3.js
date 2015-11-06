8169ed1b07fc96961127a9077c64ab6d2159a743
raskl508 <raskl508@student.liu.se>
2015-11-06 11:21:35 +0100

    new file d3.js

---
+++ public/js/directive/d3.js
@@ -0,0 +1,372 @@
+/**
+ * Created by rasmus on 2015-08-27.
+ */
+
+app.directive('testChart', function($window){
+    function link(scope, elem, attrs) {
+        scope.padding = 40;
+        scope.pathClass = "path";
+
+        var d3 = $window.d3;
+        var rawSvg = elem.find("svg")[0];
+        var svg = d3.select(rawSvg);
+
+        var totalProjectTime = 100;
+        var numberOfInputs = -1;
+        var xValues = getX(), yValues = scope.y;
+
+
+        console.log(scope.x);
+
+
+        function getX(){
+            var x = null;
+            if (scope.x) {
+                x = scope.x;
+            } else {
+                x = _.keys(scope.y);
+            }
+            return x;
+        }
+
+
+        function setChartParameters() {
+
+
+            xScale = d3.scale.linear()
+                .domain([0, scope.x - 1])
+                .range([scope.padding + 5, rawSvg.clientWidth - scope.padding]);
+
+            yScale = d3.scale.linear()
+                .domain([0, totalProjectTime])
+                .range([rawSvg.clientHeight - scope.padding, 0]);
+
+            xAxisGen = d3.svg.axis()
+                .scale(xScale)
+                .orient("bottom")
+                .ticks(xValues.length - 1);
+
+            yAxisGen = d3.svg.axis()
+                .scale(yScale)
+                .orient("left")
+                .ticks(5);
+
+            newProjectTime = totalProjectTime;
+            lineFun = d3.svg.line()
+                .x(function (d) {
+                    numberOfInputs = numberOfInputs + 1;
+                    return xScale(numberOfInputs);
+                })
+                .y(function (d) {
+                    newProjectTime = newProjectTime - d.time;
+                    return yScale(newProjectTime);
+                })
+                .interpolate("basis");
+        }
+
+
+        function drawLineChart() {
+
+            setChartParameters();
+
+            svg.append("svg:g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(0,180)")
+                .call(xAxisGen);
+
+
+            svg.append("svg:g")
+                .attr("class", "y axis")
+                .attr("transform", "translate(40,0)")
+                .call(yAxisGen);
+
+            svg.append("svg:path")
+                .attr({
+                    d: lineFun(xValues),
+                    "stroke": "blue",
+                    "stroke-width": 2,
+                    "fill": "none",
+                    "class": scope.pathClass
+                });
+        }
+
+
+        drawLineChart();
+    }
+    return {
+        restrict: 'EA',
+        template: "<svg width='850' height='220'></svg>",
+        link: link
+    }
+});
+
+
+
+app.directive('myChart', function(){
+    var directive = { };
+
+    directive.scope = {
+        x: '=?',
+        y: '=myChart',
+        options: '=?',
+        project: '=?'
+    };
+
+    directive.restrict = 'AE';
+
+
+    directive.link = function(scope, elements, attr) {
+        scope.svg = null;
+        scope.container = null;
+
+        console.log(scope.x)
+
+        scope.getX = function() {
+            var x = null;
+            if (scope.x) {
+                x = scope.x;
+            } else {
+                x = _.keys(scope.y);
+            }
+            return x;
+        };
+        /*
+        scope.getX = function() {
+            var x = null;
+                x = scope.x;
+            return x;
+        };
+*/
+
+        //Optional. If there is no option set this will be the standard:
+        scope.getOptions = function() {
+            return _.merge({
+                width: 1000,
+                height: 400,
+                margin: {
+                    top: 10,
+                    right: 10,
+                    bottom: 30,
+                    left: 50
+                }
+            }, scope.options || { });
+        };
+
+        scope.initialize = function() {
+            scope.svg = d3.select(elements[0]).append("svg").attr("class", "chart");
+            scope.container = scope.svg.append("g");
+            scope.container.append("g").attr("class", "x");
+            scope.container.append("g").attr("class", "y");
+            scope.setSvgSize();
+        };
+        scope.setSvgSize = function() {
+            var options = scope.getOptions();
+            scope.container.attr("transform", "translate(" + options.margin.left + ", " + options.margin.right + ")");
+            scope.svg.attr('viewBox','0 0 '+ (options.width + options.margin.left + options.margin.right) + ' ' +
+            (options.height + options.margin.top + options.margin.bottom))
+                .attr('preserveAspectRatio','xMinYMin');
+            scope.redraw();
+        };
+
+
+        scope.redraw = function() {
+
+            var dataset, options = scope.getOptions(), xValues = scope.getX(), yValues = scope.y;
+            console.log(xValues)
+            console.log(yValues)
+            //okey, so no I got two arrays with values from X- and Y-Axis
+
+            //get unique projects
+            //let's say project number is 1:
+
+            var lol = [];
+            var pos = 0;
+            var position;
+            while (pos < xValues.length){
+                position = xValues.indexOf(1, pos);
+                if (position != -1){
+                    lol.push(position)
+                    pos = position + 1;
+                }
+                else{
+                    break;
+                }
+            }
+
+// This is starting to get messy I can tell....
+            dataset = scope.container.selectAll(".bar").data(yValues);
+            console.log("dataset:")
+            console.log(dataset);
+
+
+            dataset.enter()
+                .append("svg:svg")
+                .attr("width", 400)
+                .attr("height", 200)
+
+            /*dataset.enter().append("rect").attr("class", "bar");
+            dataset.transition().attr("x", function(d, i) {
+                return i * x.rangeBand();
+            }).attr("width", function() {
+                return x.rangeBand() - 5;
+            }).attr("height", function(d) {
+                return options.height - y(d);
+            }).attr("y", function(d) {
+                return y(d);
+            });
+
+            dataset.exit().remove();
+*/
+
+
+
+
+
+
+            console.log("output should be: 8,9,10")
+            console.log(lol)
+
+            //paint them
+
+
+
+
+
+        };
+
+        scope.initialize();
+
+        scope.$watch('x', scope.redraw);
+        scope.$watch('y', scope.redraw);
+        scope.$watch('options', scope.setSvgSize);
+
+
+    };
+        return directive;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.directive('barChart', function(){
+ // angular.module("d3.directives", []).directive("barChart", function() {
+    var directive = { };
+
+    directive.restrict = 'AE';
+    directive.scope = {
+        x: '=?',
+        y: '=barChart',
+        options: '=?',
+        project: '=?'
+    };
+
+    directive.link = function(scope, elements, attr) {
+        scope.svg = null;
+        scope.container = null;
+
+        scope.getX = function() {
+            var x = null;
+            if (scope.x) {
+                x = scope.x;
+            } else {
+                x = _.keys(scope.y);
+            }
+            return x;
+        };
+
+        scope.getOptions = function() {
+            return _.merge({
+                width: 1000,
+                height: 500,
+                margins: {
+                    top: 20,
+                    right: 20,
+                    bottom: 20,
+                    left: 50
+                }
+            }, scope.options || { });
+        };
+
+        scope.initialize = function() {
+            scope.svg = d3.select(elements[0]).append("svg").attr("class", "chart");
+            scope.container = scope.svg.append("g");
+            scope.container.append("g").attr("class", "x");
+            scope.container.append("g").attr("class", "y");
+            scope.setSvgSize();
+        };
+
+        scope.setSvgSize = function() {
+            var options = scope.getOptions();
+            scope.container.attr("transform", "translate(" + options.margins.left + ", " + options.margins.right + ")");
+            scope.svg.attr('viewBox','0 0 '+ (options.width + options.margins.left + options.margins.right) + ' ' +
+            (options.height + options.margins.top + options.margins.bottom))
+                .attr('preserveAspectRatio','xMinYMin');
+            scope.redraw();
+        };
+
+        scope.redraw = function() {
+            var x, y, xAxis, yAxis, dataset, options = scope.getOptions(), xValues = scope.getX(), yValues = scope.y;
+
+            console.log(xValues)
+            var lineGen = d3.svg.line()
+                .x(function (d, i) {
+                    return xScale(i);
+                })
+                .y(function (d) {
+                    return yScale(d);
+//                    startTime = startTime - d;
+ //                   return yScale(startTime);
+                })
+                .interpolate("crispEdges");
+
+            if (xValues && yValues) {
+                x = d3.scale.ordinal().domain(xValues).rangeRoundBands([ 0, options.width ], 0);
+                y = d3.scale.linear().domain([0, d3.max(yValues)]).range([ options.height, 0]);
+                xAxis = d3.svg.axis().scale(x).orient("bottom").ticks(5);
+                yAxis = d3.svg.axis().scale(y).orient("left").ticks(10);
+
+                scope.container.selectAll("g.x").attr("transform", "translate(0, " + options.height + ")").call(xAxis);
+                scope.container.selectAll("g.y").call(yAxis);
+
+/*
+                dataset.append('svg:path')
+                    .attr('d', lineGen(x))
+                    .attr('stroke', function() { "green"})
+                    .attr('stroke-width', 2)
+                    .attr('fill', 'none');
+*/
+                dataset = scope.container.selectAll(".bar").data(yValues);
+                console.log(dataset);
+                dataset.enter().append("rect").attr("class", "bar");
+                dataset.transition().attr("x", function(d, i) {
+                    return i * x.rangeBand();
+                }).attr("width", function() {
+                    return x.rangeBand() - 5;
+                }).attr("height", function(d) {
+                    return options.height - y(d);
+                }).attr("y", function(d) {
+                    return y(d);
+                });
+
+                dataset.exit().remove();
+            }
+        };
+
+        scope.$watch('x', scope.redraw);
+        scope.$watch('y', scope.redraw);
+        scope.$watch('options', scope.setSvgSize);
+
+        scope.initialize();
+    };
+
+    return directive;
+});
