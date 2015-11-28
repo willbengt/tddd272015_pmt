8af919e61b5944c5341ffd3c4eef61321ee9e876
raskl508 <raskl508@student.liu.se>
2015-11-28 15:36:19 +0100

    added migration email to tokens

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151128141827) do
+ActiveRecord::Schema.define(version: 20151128143514) do
 
   create_table "people", force: :cascade do |t|
     t.string   "name"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20151128141827) do
     t.datetime "expires_at"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "email"
   end
 
 end
