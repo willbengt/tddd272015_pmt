1456aacfeea7cf4ee86e274bf8fa681c21aedc6d
William Bengtsson <wilbe075@student.liu.se>
2015-12-12 10:55:39 +0100

    Destroyed db and remigrated with updated migration records

--- db/migrate/20151114155020_add_time_to_projects.rb
+++
@@ -1,5 +0,0 @@
-class AddTimeToProjects < ActiveRecord::Migration
-  def change
-  	add_column :projects, :time, :float
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151212085120) do
+ActiveRecord::Schema.define(version: 20151212095022) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "name"
+    t.float    "time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.float    "time"
   end
 
   create_table "reports", force: :cascade do |t|
