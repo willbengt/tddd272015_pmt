4ef8647384938a95802f0da2e0be93fd23a95329
William Bengtsson <wilbe075@student.liu.se>
2016-01-06 12:11:20 +0100

    Added ResourceInterceptor Service

---
+++ public/js/services/resourceInterceptor.js
@@ -0,0 +1,16 @@
+app.factory('ResourceInterceptor', function() {
+
+  var rootUrl = "http://localhost:3000";
+  //var rootUrl = "http://127.0.0.1:3000/";
+    
+  return {
+    response: function (data) {
+      console.log("success (" + data.config.method + ' ' + rootUrl + data.config.url + ")", data);
+      return data;
+    },
+    responseError: function (data) {
+      console.log("success (" + data.config.method + ' ' + rootUrl + data.config.url + ")", data);
+      return data;
+    }
+  };
+});
