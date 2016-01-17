9ebc40584e4a7fedd8d326c8d7da1a4308df175e
William Bengtsson <wilbe075@student.liu.se>
2016-01-17 11:18:47 +0100

    Added upadateChartData function

--- public/js/controllers/ProjectController.js
+++ public/js/controllers/ProjectController.js
@@ -1,6 +1,7 @@
 app.controller('ProjectController', ['$scope', '$rootScope', '$stateParams', '$filter', 'Report', 'Project', 
 function($scope, $rootScope, $stateParams, $filter, Report, Project) {
 
+  /*
   loadReports = function() {
     Report.query(function(response) {
         $rootScope.reports = $filter('filter')(response, {project: $stateParams.projectId});
@@ -13,6 +14,23 @@ function($scope, $rootScope, $stateParams, $filter, Report, Project) {
         });
     });
   };
+*/
+
+        updateChartData = function() {
+
+          var reports = $rootScope.reports;
+          
+          $scope.set = {
+              time: [],
+              x: [],
+              y: []
+          };
+
+          for (var i = 0; i < reports.length; i++) {
+            $scope.set.x.push(reports[i].id);
+            reports[i].time ? $scope.set.y.push(reports[i].time) : $scope.set.y.push(0);
+          };  
+        }
 
         $scope.init = function() {
             $scope.project = Project.get({
@@ -20,20 +38,27 @@ function($scope, $rootScope, $stateParams, $filter, Report, Project) {
                     user: window.localStorage.user_name.slice(1, -1),
                     token: window.localStorage.access_token.slice(1, -1)},
                 function(resource) {
-                    $scope.prjtime = resource.time;
+                  Report.query(function(response) {
+                    $rootScope.reports = $filter('filter')(response, {project: $stateParams.projectId});
+                    updateChartData();
+                  });
                 });
-            loadReports();
+            //loadReports();
         };
 
         $scope.saveReport = function(elementData, elementId) {
             var report = new Report();
             angular.extend(report, {id: elementId, project: $stateParams.projectId}, elementData);
-            report.$update();
+            report.$update(function() {
+              updateChartData();
+            });
         };
 
         $scope.removeReport = function(report, rowIndex){
             $rootScope.reports.splice(rowIndex, 1);
-            report.$delete();
+            report.$delete(function() {
+              updateChartData();
+            });
         };
 
         $scope.addReport = function() {
@@ -44,9 +69,10 @@ function($scope, $rootScope, $stateParams, $filter, Report, Project) {
                 $rootScope.reports.push($scope.inserted);
             });
         };
-
+/*
         $scope.set = {
             y: [],
             x: []
         };
+        */
     }]);
