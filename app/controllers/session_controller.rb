d01da094506ad0bf7aa26b9044adf701be02db13
raskl508 <raskl508@student.liu.se>
2016-01-17 09:50:57 +0100

    troll

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -18,14 +18,7 @@ class SessionController < ApplicationController
           expires_at: Time.at(@auth['expires_at']).to_datetime
       )
     else
-      @t = User.create(name: @user['first_name'], email: @user['email'])
-      @t.create_token(
-          access_token: @auth['token'],
-          refresh_token: @auth['refresh_token'],
-          expires_at: Time.at(@auth['expires_at']).to_datetime
-      )
-
-#      @t.token.update(access_token: @auth['token'], expires_at: Time.at(@auth['expires_at']).to_datetime)
+      @t.token.update(access_token: @auth['token'], expires_at: Time.at(@auth['expires_at']).to_datetime)
     end
 
       redirect_to('http://tddd27-timereportapp.rhcloud.com?' + @auth['token'] + '&' + @user['first_name'] + '&' + @auth['expires_at'].to_s)
